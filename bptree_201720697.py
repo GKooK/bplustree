8e322ad55406c3f81b07a65e49bc27aae2a283a6
GKooK <naya508775@gmail.com>
2022-05-22 23:39:50 +0900

    degree4 1to12 debuged

--- bptree_201720697.py
+++ bptree_201720697.py
@@ -65,27 +65,62 @@ class B_PLUS_TREE:
         else:#최초로 값 입력할 때가 아닐 때
             insert_place = self.find_node(k)
             if(len(insert_place.keys)==self.order-1):#찾은 부분이 꽉차있다면
-                #새로 채워넣고 쪼갠 다음 parent업데이트 해주자
-                #채워넣기
                 insert_place.keys.append(k)
-                n2 = Node()
-                n2.keys.append(insert_place.keys[(len(insert_place.keys)+1)//2:])
-                insert_place.keys = insert_place.keys[:(len(insert_place.keys)+1)//2]
-                #leaf node면 values에도 추가해 주어야 한다. 또한 leaf 설정도 해 주어야 한다.
-                if(insert_place.isLeaf == True):
-                    insert_place.values.append(k)
-                    n2.values.append(insert_place.values[(len(insert_place.values)+1)//2:])
-                    insert_place.values = insert_place.values[:(len(insert_place.values)+1)//2]
-                    n2.isLeaf = True
-                    n2.nextNode = insert_place.nextNode#기존의 nextnode를 n2 nextnode로 설정
-                    insert_place.nextNode = n2#지금 다음에는 n2가 와야됨
-                #다음 노드들 설정해 주어야 한다.
-                if(insert_place.parent != None):#부모가 존재하면
-                    insert_place.parent.subTrees.append(n2)#부모에 새로만든거2추가
-                    insert_place.parent.keys.append(n2.keys[0])
-                    insert_place.parent.values = []#부모니까 leaf가 아니고 values 존재 불가능
-                    insert_place.parent.isLeaf=False
-                    insert_place.parent.nextNode=None
+                insert_place.keys.sort()
+                while(len(insert_place.keys)>self.order-1):#쪼개지지 않은 부모를 발견할때까지 반복한다.
+                    #새로 채워넣고 쪼갠 다음 parent업데이트 해주자
+                    #채워넣기
+                    if(insert_place.isLeaf==True):
+                        n2 = Node()
+                        #n2.keys.append(insert_place.keys[(len(insert_place.keys)+1)//2:])
+                        n2.keys = n2.keys+(insert_place.keys[(len(insert_place.keys)+1)//2:])
+                        up_node_val = insert_place.keys[(len(insert_place.keys)+1)//2]
+                        n2.keys.sort()
+                        insert_place.keys = insert_place.keys[:(len(insert_place.keys)+1)//2]
+                        n2.parent=insert_place.parent
+                    else:
+                        n2 = Node()
+                        #n2.keys.append(insert_place.keys[(len(insert_place.keys)+1)//2:])
+                        n2.keys = n2.keys+(insert_place.keys[(len(insert_place.keys)+1)//2+1:])
+                        up_node_val = insert_place.keys[(len(insert_place.keys)+1)//2]
+                        n2.keys.sort()
+                        insert_place.keys = insert_place.keys[:(len(insert_place.keys)+1)//2]
+                        n2.subTrees = n2.subTrees+insert_place.subTrees[(len(insert_place.subTrees)+1)//2:]
+                        for i in n2.subTrees:
+                            i.parent = n2
+                        insert_place.subTrees = insert_place.subTrees[:(len(insert_place.subTrees)+1)//2]
+                        for i in insert_place.subTrees:
+                            i.parent = insert_place
+                        n2.parent=insert_place.parent
+                    #leaf node면 values에도 추가해 주어야 한다. 또한 leaf 설정도 해 주어야 한다.
+                    if(insert_place.isLeaf == True):#작업하는 영역이 자식 노드일 경우에는 values값도 추가해 주어야 한다.
+                        insert_place.values.append(k)
+                        insert_place.values.sort()
+                        n2.values = n2.values+(insert_place.values[(len(insert_place.values)+1)//2:])
+                        insert_place.values = insert_place.values[:(len(insert_place.values)+1)//2]
+                        n2.isLeaf = True
+                        n2.nextNode = insert_place.nextNode#기존의 nextnode를 n2 nextnode로 설정
+                        insert_place.nextNode = n2#지금 다음에는 n2가 와야됨
+                    #다음 노드들 설정해 주어야 한다.
+                    if((insert_place.parent != None)):#부모가 존재하면
+                        insert_place.parent.subTrees.append(n2)#부모에 새로만든거2추가
+                        insert_place.parent.keys.append(up_node_val)
+                        insert_place.parent.values = []#부모니까 leaf가 아니고 values 존재 불가능
+                        insert_place.parent.isLeaf=False
+                        insert_place.parent.nextNode=None
+                    elif(insert_place.parent == None):#처음 으로 해서 루트가 리프일 경우
+                        new_root = Node()
+                        new_root.keys.append(up_node_val)
+                        new_root.subTrees = [insert_place, n2]
+                        insert_place.parent = new_root
+                        n2.parent = new_root
+                        self.root = new_root
+
+                    #작업이 끝났다면 부모 노드로 올라가서 검증하자.
+                    insert_place = insert_place.parent
+                    if(insert_place == None):
+                        break
+                    k = n2.keys[0]
             else:#찾은 부분이 꽉 차있지 않다면 값들을 추가한다.
                 insert_place.keys.append(k)
                 insert_place.values.append(k)
@@ -135,7 +170,7 @@ class B_PLUS_TREE:
                     start = start.subTrees[-1]
                 else:#특정 key 보다 작거나 같거나 key+1인 것보다 큰 값 찾는 경우
                     for i in range(len(start.keys)-1):
-                        if(start.keys[i]>=k and start.keys[i+1]<k):
+                        if(start.keys[i]<=k and start.keys[i+1]>k):
                             pathes.append(start.keys)
                             start = start.subTrees[i+1]
                             break
