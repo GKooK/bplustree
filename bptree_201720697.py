afdc1df7afccb6ba3262dcc7024edd89e4cff43a
GKooK <76737552+GKooK@users.noreply.github.com>
2022-05-22 15:42:49 +0900

    Add files via upload

---
+++ bptree_201720697.py
@@ -0,0 +1,200 @@
+from logging import root
+import sys
+
+
+class Node:
+    def __init__(self):
+        # each node can have |order - 1| keys
+        self.keys = []
+        
+        # |order| / 2 <= # of subTree pointers <= |order|
+        self.subTrees = []
+        
+        self.parent = None
+        self.isLeaf = False
+        
+        # leaf node has next node pointer
+        self.nextNode = None   
+        self.values = []
+
+
+class B_PLUS_TREE:
+    '''
+    Implement below functions
+    '''
+    def __init__(self, order):
+        self.order = order
+        self.root  = None
+        pass      
+        
+    def find_node(self, k):
+        if(self.root.isLeaf == True):
+            return self.root
+        else:
+            start = self.root
+            pathes = []
+            return_val = None
+            while(1):
+                if(start.isLeaf==True):#최종 리프 노드까지 오게 되면
+                    pathes.append(start.keys)
+                    return_val = start
+                    break
+                if(start.keys[0] > k): #제일 작은 key보다 작은은 값을 찾을 경우
+                    pathes.append(start.keys)
+                    start = start.subTrees[0]
+                elif(start.keys[-1] <= k): #제일 큰 key보다 크거나 같은 값을 찾을 경우
+                    pathes.append(start.keys)
+                    start = start.subTrees[-1]
+                else:#특정 key 보다 작거나 같거나 key+1인 것보다 큰 값 찾는 경우
+                    for i in range(len(start.keys)-1):
+                        if(start.keys[i]>=k and start.keys[i+1]<k):
+                            pathes.append(start.keys)
+                            start = start.subTrees[i+1]
+                            break
+            return return_val
+        return None
+        pass
+
+    def insert(self, k):        
+        if(self.root==None):#최초로 값 삽입할 때
+            n = Node()
+            n.keys.append(k)
+            n.values.append(k)
+            self.root = n
+            self.root.isLeaf = True
+        else:#최초로 값 입력할 때가 아닐 때
+            insert_place = self.find_node(k)
+            if(len(insert_place.keys)==self.order-1):#찾은 부분이 꽉차있다면
+                #새로 채워넣고 쪼갠 다음 parent업데이트 해주자
+                #채워넣기
+                insert_place.keys.append(k)
+                n2 = Node()
+                n2.keys.append(insert_place.keys[(len(insert_place.keys)+1)//2:])
+                insert_place.keys = insert_place.keys[:(len(insert_place.keys)+1)//2]
+                #leaf node면 values에도 추가해 주어야 한다. 또한 leaf 설정도 해 주어야 한다.
+                if(insert_place.isLeaf == True):
+                    insert_place.values.append(k)
+                    n2.values.append(insert_place.values[(len(insert_place.values)+1)//2:])
+                    insert_place.values = insert_place.values[:(len(insert_place.values)+1)//2]
+                    n2.isLeaf = True
+                    n2.nextNode = insert_place.nextNode#기존의 nextnode를 n2 nextnode로 설정
+                    insert_place.nextNode = n2#지금 다음에는 n2가 와야됨
+                #다음 노드들 설정해 주어야 한다.
+                if(insert_place.parent != None):#부모가 존재하면
+                    insert_place.parent.subTrees.append(n2)#부모에 새로만든거2추가
+                    insert_place.parent.keys.append(n2.keys[0])
+                    insert_place.parent.values = []#부모니까 leaf가 아니고 values 존재 불가능
+                    insert_place.parent.isLeaf=False
+                    insert_place.parent.nextNode=None
+            else:#찾은 부분이 꽉 차있지 않다면 값들을 추가한다.
+                insert_place.keys.append(k)
+                insert_place.values.append(k)
+
+        pass
+    
+    def delete(self, k):
+
+        pass
+    
+    def print_root(self):
+        l = "["
+        for k in self.root.keys:
+            l += "{},".format(k)
+        l = l[:-1] + "]"
+        print(l)
+        pass
+    
+    def print_tree(self):
+        queue = []
+        queue.append(self.root)
+        while(len(queue)!=0):
+            a=1
+        pass
+        
+    def find_range(self, k_from, k_to):
+        pass
+        
+    def find(self, k):
+        if(self.root.isLeaf == True):
+            print(self.root.keys)
+            return self.root
+        else:
+            start = self.root
+            pathes = []
+            return_val = None
+            while(1):
+                if(start.isLeaf==True):#최종 리프 노드까지 오게 되면
+                    pathes.append(start.keys)
+                    return_val = start
+                    break
+                if(start.keys[0] > k):#제일 작은 key보다 작은은 값을 찾을 경우
+                    pathes.append(start.keys)
+                    start = start.subTrees[0]
+                elif(start.keys[-1] <= k): #제일 큰 key보다 크거나 같은 값을 찾을 경우
+                    pathes.append(start.keys)
+                    start = start.subTrees[-1]
+                else:#특정 key 보다 작거나 같거나 key+1인 것보다 큰 값 찾는 경우
+                    for i in range(len(start.keys)-1):
+                        if(start.keys[i]>=k and start.keys[i+1]<k):
+                            pathes.append(start.keys)
+                            start = start.subTrees[i+1]
+                            break
+            print(pathes)
+            return return_val
+        return None
+        pass
+
+
+def main():
+    '''
+    Input: test_bp.txt
+    Output: result.txt
+    '''
+    sys.stdin = open("test_bp.txt",'r')
+    sys.stdout = open("result.txt","w")
+    myTree = None
+    
+    while (True):
+        comm = sys.stdin.readline()
+        comm = comm.replace("\n", "")
+        params = comm.split()
+        if len(params) < 1:
+            continue
+        
+        print(comm)
+        
+        if params[0] == "INIT":
+            order = int(params[1])
+            myTree = B_PLUS_TREE(order)
+            
+        elif params[0] == "EXIT":
+            return
+            
+        elif params[0] == "INSERT":
+            k = int(params[1])
+            myTree.insert(k)
+            
+        elif params[0] == "DELETE":
+            k = int(params[1])
+            myTree.delete(k)            
+            
+        elif params[0] == "ROOT":            
+            myTree.print_root()            
+            
+        elif params[0] == "PRINT":            
+            myTree.print_tree()            
+                  
+        elif params[0] == "FIND":            
+            k = int(params[1])
+            myTree.find(k)
+            
+        elif params[0] == "RANGE":            
+            k_from = int(params[1])
+            k_to = int(params[2])
+            myTree.find_range(k_from, k_to)
+        
+        elif params[0] == "SEP":
+            print("-------------------------")
+    
+if __name__ == "__main__":
+    main()
---
+++ for_test.py
@@ -0,0 +1,8 @@
+from bptree_201720697 import Node
+from bptree_201720697 import B_PLUS_TREE as btree
+
+temp = btree(4)
+temp.insert(1)
+temp.insert(2)
+temp.insert(3)
+temp.insert(4)
